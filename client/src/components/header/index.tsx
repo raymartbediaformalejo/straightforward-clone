7b3f24358dd6e43c39f1d809e3b7ebed623a3745
Raymart <100412726+raymartbediaformalejo@users.noreply.github.com>
2024-03-10 23:53:20 +0800

    feat: Implement Header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,17 +1,74 @@
 import { Menu, ShoppingCart } from "lucide-react";
 import Logo from "../svg/Logo";
+import { useEffect, useRef, useState } from "react";
+import { cn } from "@/lib/utils";
 
 const Header = () => {
+  const headerRef = useRef<HTMLElement>(null);
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [isPointerInHeader, setIsPointerInHeader] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsPointerInHeader(true);
+  };
+  const handleMouseLeave = () => {
+    setIsPointerInHeader(false);
+  };
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > 0 || isPointerInHeader) {
+        setIsScrolling(true);
+      } else {
+        setIsScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isPointerInHeader]);
+
   return (
     <div className="sticky top-0 left-0 w-full z-10 mb-[-61px]">
-      <header className="block text-white bg-transparent ">
+      <header
+        ref={headerRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className={cn(
+          "block text-white transition-[background-color,box-shadow] duration-300 ease-in-out",
+          isScrolling || isPointerInHeader ? "bg-white" : "bg-transparent"
+        )}
+      >
         <div className=" flex items-center justify-between px-[15px] py-[17.5px]">
           <button>
-            <Menu size="24px" />
+            <Menu
+              size="24px"
+              className={cn(
+                "transition-[color] duration-300 ease-in-out",
+                isScrolling || isPointerInHeader ? "text-primary" : "text-white"
+              )}
+            />
           </button>
-          <Logo className="fill-white" />
+          <Logo
+            className={cn(
+              "transition-[fill] duration-300 ease-in-out",
+              isScrolling || isPointerInHeader ? "fill-primary" : "fill-white"
+            )}
+          />
           <button>
-            <ShoppingCart />
+            <ShoppingCart
+              size="24px"
+              className={cn(
+                "transition-[color] duration-300 ease-in-out",
+                isScrolling || isPointerInHeader ? "text-primary" : "text-white"
+              )}
+            />
           </button>
         </div>
       </header>
